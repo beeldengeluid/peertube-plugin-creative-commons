9d438525cc9f998082f77b2e4f20d64c177c1411
frankstrater <frankstrater@gmail.com>
2021-06-08 13:48:39 +0200

    Simplify videoLicenceManager usage

--- main.js
+++ main.js
@@ -3,53 +3,22 @@ async function register ({
   videoLicenceManager,
 }) {
 
-  const CC_VIDEO_LICENCES = {
-    1: {
-      label: "CC BY 4.0",
-      image: "https://licensebuttons.net/l/by/4.0/80x15.png",
-      href: "https://creativecommons.org/licenses/by/4.0/" 
-    },
-    2: {
-      label: "CC BY-SA 4.0",
-      image: "https://licensebuttons.net/l/by-sa/4.0/80x15.png",
-      href: "https://creativecommons.org/licenses/by-sa/4.0/" 
-    },
-    3: {
-      label: "CC BY-ND 4.0",
-      image: "https://licensebuttons.net/l/by-nd/4.0/80x15.png",
-      href: "https://creativecommons.org/licenses/by-nd/4.0/" 
-    },
-    4: {
-      label: "CC BY-NC 4.0",
-      image: "https://licensebuttons.net/l/by-nc/4.0/80x15.png",
-      href: "https://creativecommons.org/licenses/by-nc/4.0/" 
-    },
-    5: {
-      label: "CC BY-NC-SA 4.0",
-      image: "https://licensebuttons.net/l/by-nc-sa/4.0/80x15.png",
-      href: "https://creativecommons.org/licenses/by-nc-sa/4.0/" 
-    },
-    6: {
-      label: "CC BY-NC-ND 4.0",
-      image: "https://licensebuttons.net/l/by-nc-nd/4.0/80x15.png",
-      href: "https://creativecommons.org/licenses/by-nc-nd/4.0/" 
-    },
-    7: {
-      label: "CC0 1.0",
-      image: "https://licensebuttons.net/l/zero/1.0/80x15.png",
-      href: "https://creativecommons.org/publicdomain/zero/1.0/" 
-    },
-    8: {
-      label: "Public Domain Mark 1.0",
-      image: "https://licensebuttons.net/l/publicdomain/80x15.png",
-      href: "https://creativecommons.org/publicdomain/mark/1.0/"
-    }
-  }
-
-  for(i = 1;i <= 8;i++) {
-    videoLicenceManager.deleteLicence(i)
-    videoLicenceManager.addLicence(i, CC_VIDEO_LICENCES[i].label)
-  }
+  videoLicenceManager.deleteLicence(1)
+  videoLicenceManager.deleteLicence(2)
+  videoLicenceManager.deleteLicence(3)
+  videoLicenceManager.deleteLicence(4)
+  videoLicenceManager.deleteLicence(5)
+  videoLicenceManager.deleteLicence(6)
+  videoLicenceManager.deleteLicence(7)
+  
+  videoLicenceManager.addLicence(1, 'CC BY 4.0')
+  videoLicenceManager.addLicence(2, 'CC BY-SA 4.0')
+  videoLicenceManager.addLicence(3, 'CC BY-ND 4.0')
+  videoLicenceManager.addLicence(4, 'CC BY-NC 4.0')
+  videoLicenceManager.addLicence(5, 'CC BY-NC-SA 4.0')
+  videoLicenceManager.addLicence(6, 'CC BY-NC-ND 4.0')
+  videoLicenceManager.addLicence(7, 'CC0 1.0')
+  videoLicenceManager.addLicence(8, 'Public Domain Mark 1.0')
 
 }
 

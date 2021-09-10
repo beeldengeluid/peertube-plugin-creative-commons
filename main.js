c2d91714e117e3a5f63f8d7dec6d5a7e2f172839
frankstrater <frankstrater@gmail.com>
2021-09-10 13:07:27 +0200

    Replace deprecated videoLicenceManager functions for 3.4

--- main.js
+++ main.js
@@ -3,22 +3,22 @@ async function register ({
   videoLicenceManager,
 }) {
 
-  videoLicenceManager.deleteLicence(1)
-  videoLicenceManager.deleteLicence(2)
-  videoLicenceManager.deleteLicence(3)
-  videoLicenceManager.deleteLicence(4)
-  videoLicenceManager.deleteLicence(5)
-  videoLicenceManager.deleteLicence(6)
-  videoLicenceManager.deleteLicence(7)
+  videoLicenceManager.deleteConstant(1)
+  videoLicenceManager.deleteConstant(2)
+  videoLicenceManager.deleteConstant(3)
+  videoLicenceManager.deleteConstant(4)
+  videoLicenceManager.deleteConstant(5)
+  videoLicenceManager.deleteConstant(6)
+  videoLicenceManager.deleteConstant(7)
   
-  videoLicenceManager.addLicence(1, 'CC BY 4.0')
-  videoLicenceManager.addLicence(2, 'CC BY-SA 4.0')
-  videoLicenceManager.addLicence(3, 'CC BY-ND 4.0')
-  videoLicenceManager.addLicence(4, 'CC BY-NC 4.0')
-  videoLicenceManager.addLicence(5, 'CC BY-NC-SA 4.0')
-  videoLicenceManager.addLicence(6, 'CC BY-NC-ND 4.0')
-  videoLicenceManager.addLicence(7, 'CC0 1.0')
-  videoLicenceManager.addLicence(8, 'Public Domain Mark 1.0')
+  videoLicenceManager.addConstant(1, 'CC BY 4.0')
+  videoLicenceManager.addConstant(2, 'CC BY-SA 4.0')
+  videoLicenceManager.addConstant(3, 'CC BY-ND 4.0')
+  videoLicenceManager.addConstant(4, 'CC BY-NC 4.0')
+  videoLicenceManager.addConstant(5, 'CC BY-NC-SA 4.0')
+  videoLicenceManager.addConstant(6, 'CC BY-NC-ND 4.0')
+  videoLicenceManager.addConstant(7, 'CC0 1.0')
+  videoLicenceManager.addConstant(8, 'Public Domain Mark 1.0')
 
 }
 
